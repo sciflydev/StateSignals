06b5c1cea15699b768c99c0b167afbffa5320d74
PGimenez <gimenezfebrer@gmail.com>
2024-11-30 19:14:28 +0100

    run effect on signal pull

--- src/Signals.jl
+++ src/Signals.jl
@@ -1,7 +1,7 @@
 module Signals
 
 export Signal, computed, effect, invalidate, pull!
-export CONTEXT_SIGNALS
+export COMPUTED_DEPS
 CONTEXT::Union{Function,Nothing} = nothing
 
 mutable struct Signal{T}
@@ -12,7 +12,7 @@ mutable struct Signal{T}
     effects::Set{Function}
 end
 
-CONTEXT_SIGNALS::Set{Signal} = Set{Signal}()
+COMPUTED_DEPS::Set{Signal} = Set{Signal}()
 
 Signal(x::Number) = Signal(x, true, nothing, Set{Signal}(), Set{Function}())
 # Signal(f,x) = Signal(x, true, f, Set{Signal}(), Set{Function}())
@@ -23,33 +23,36 @@ end
 
 # x() = s()
 function computed(f::Function)
-    global CONTEXT_SIGNALS
-    CONTEXT_SIGNALS = Set{Signal}()
+    global COMPUTED_DEPS
+    COMPUTED_DEPS = Set{Signal}()
     s = Signal(f)
-    for dependency in CONTEXT_SIGNALS
+    for dependency in COMPUTED_DEPS
         push!(dependency.children, s)
     end
-    CONTEXT_SIGNALS = Set{Signal}()
+    COMPUTED_DEPS = Set{Signal}()
     s
 end
 
 function (s::Signal)()
     CONTEXT != nothing && push!(s.effects, CONTEXT)
-    push!(CONTEXT_SIGNALS, s)
+    push!(COMPUTED_DEPS, s)
     s.valid && return s.value
     pull!(s)
 end
 
 function pull!(s::Signal)
-    if !s.valid
-        s.value = s.action()
-        s.valid = true
+  if !s.valid
+    s.value = s.action()
+    s.valid = true
+    for fn in s.effects
+        fn()
     end
+  end
     s.value
 end
 
 function (s::Signal)(value)
-    s.action != nothing && error("Can't modify state of computed signal.")
+    s.action != nothing && error("Can't modify state of computed signal.");
     invalidate(s)
     s.value = value
     s.valid = true
@@ -74,3 +77,5 @@ function invalidate(s::Signal)
 end
 
 end
+
+
